bfb726c45329209c54adf5add978b8d5c53ce4d0
Luiz Henrique Gomes <luiz.hgsilva@hotmail.com>
2025-05-20 20:25:18 -0300

    initial commit: injection added

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,11 +3,13 @@
 // service locator
 import 'package:get_it/get_it.dart';
 import 'package:teste_flutter/features/customers/stores/customers.store.dart';
+import 'package:teste_flutter/features/tables/stores/tables_store.dart';
 
 final sl = GetIt.I;
 
 void slStores() {
   sl.registerLazySingleton<CustomersStore>(() => CustomersStore());
+  sl.registerLazySingleton<TablesStore>(() => TablesStore());
 }
 
 void init() {
